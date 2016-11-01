1b9308da4e66fc912b8e0c89f3284268d6072fe4
Oleksandr Kruk <kruk.oleksandr@gmail.com>
2016-11-01 22:21:11 +0000

    Add implementation and tests for Deque

--- src/queues/Deque.java
+++ src/queues/Deque.java
@@ -1,6 +1,7 @@
 package queues;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * @author Oleksandr Kruk
@@ -9,16 +10,23 @@ import java.util.Iterator;
 public class Deque<Item> implements Iterable<Item> {
 
   Node<Item> first;
+  Node<Item> last;
+  int size;
 
   public Deque() {
     first = null;
+    last = first;
   }
 
   @Override
   public Iterator<Item> iterator() {
-    return null;
+    return new ListIterator(first);
   }
 
+  /**
+   * 
+   * @return {@code true} if {@link Deque} is empty, {@code false} otherwise
+   */
   public boolean isEmpty() {
     return first == null;
   }
@@ -29,7 +37,7 @@ public class Deque<Item> implements Iterable<Item> {
    * @return {@code int} number of items on the deque
    */
   public int size() {
-    return 0;
+    return size;
   }
 
   /**
@@ -38,23 +46,101 @@ public class Deque<Item> implements Iterable<Item> {
    *          to be added to Deque
    */
   public void addFirst(Item item) {
-    Node<Item> oldFirst = first;
-    first = new Node<Item>(item);
-    first.next = oldFirst;
+    if (isEmpty()) {
+      first = last =  new Node<Item>(item);
+    } else {
+      Node<Item> oldFirst = first;
+      first = new Node<Item>(item);
+      first.next = oldFirst;
+      oldFirst.previous = first;
+    }
+
+    size++;
+  }
+
+  public Item removeFirst() {
+    if (isEmpty())
+      throw new NoSuchElementException("deque is empty");
+    Item toReturn = first.value;
+
+    if (size == 1) {
+      first = last = null;
+    } else {
+      Node<Item> oldFirst = first;
+      first = oldFirst.next;
+      oldFirst.next = null;
+      first.previous = null;
+    }
+
+    size--;
+    return toReturn;
+  }
+
+  public void addLast(Item item) {
+    if (isEmpty()) {
+      first = last = new Node<Item>(item);
+    } else {
+      Node<Item> oldLast = last;
+      last = new Node<Item>(item);
+      last.previous = oldLast;
+      oldLast.next = last;
+    }
+
+    size++;
+  }
+
+  public Item removeLast() {
+    if (isEmpty())
+      throw new NoSuchElementException("deque is empty");
+
+    final Item toReturn = last.value;
+    if(size == 1) {
+      first = last = null;
+    } else {
+      Node<Item> oldLast = last;
+      last = oldLast.previous;
+      oldLast.previous = null;
+      last.next = null;
+    }
+
+    size--;
+    return toReturn;
   }
 
   public class Node<T> {
-    T value;
-    Node<T> next;
+    public T value;
+    public Node<T> next;
+    public Node<T> previous;
 
     public Node(T v) {
       value = v;
     }
   }
 
-  public Item removeFirst() {
-    Item toReturn = first.value;
-    first = first.next;
-    return toReturn;
+  public class ListIterator implements Iterator<Item> {
+
+    Node<Item> current;
+
+    public ListIterator(Node<Item> firstNode) {
+      current = firstNode;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return current.next != null;
+    }
+
+    @Override
+    public Item next() {
+      if (current.next == null)
+        throw new NoSuchElementException();
+
+      return current.next.value;
+    }
+
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
   }
 }
--- src/test/TestDeque.java
+++ src/test/TestDeque.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.NoSuchElementException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,9 +30,59 @@ public class TestDeque {
   }
 
   @Test
-  public void itShouldBeEmptyAfterAddingAndRemovingOneItem() {
+  public void itShouldBeEmptyAfterRemovingLastItem() {
     deque.addFirst("testing");
     assertFalse(deque.isEmpty());
     deque.removeFirst();
+  }
+
+  @Test
+  public void itShouldRemoveInExpectedOrder() {
+    deque.addFirst("test");
+    deque.addFirst("test2");
+    assertEquals(deque.removeFirst(), "test2");
+    assertEquals(deque.removeFirst(), "test");
+  }
+
+  @Test(expected=NoSuchElementException.class)
+  public void itThrowsNoSuchElementWhenRemoveFromEmptyDeque() {
+    deque.removeFirst();
+  }
+
+  @Test
+  public void itShouldAddItemAtTheEndWhenAddLast() {
+    deque.addLast("test");
+    deque.addLast("test2");
+    assertEquals(deque.size(), 2);
+    assertEquals(deque.removeFirst(), "test");
+    assertEquals(deque.removeFirst(), "test2");
+  }
+
+  @Test
+  public void itShouldRemoveInFifoOrder() {
+    deque.addLast("test");
+    deque.addLast("test2");
+    assertEquals(deque.size(), 2);
+    assertEquals(deque.removeLast(), "test2");
+    assertEquals(deque.removeLast(), "test");
+  }
+
+  @Test
+  public void itShouldUpdateSizeCorrectly() {
+    deque.addLast("test");
+    deque.addFirst("test2");
+    assertEquals(deque.size(), 2);
+    deque.removeLast();
+    assertEquals(deque.size(), 1);
+    deque.removeFirst();
+    assertEquals(deque.size(), 0);
+  }
+
+  @Test
+  public void itShouldWorkCorrectlyWhenAddingAfterEmptyingDeque() {
+    deque.addFirst("test");
+    deque.removeFirst();
+    deque.addFirst("test2");
+    assertEquals(deque.removeFirst(), "test2");
   }
 }
